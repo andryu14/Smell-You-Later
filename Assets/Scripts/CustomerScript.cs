5e650b150ddcf74ba52a506281620cd5d898cf00
Waddy Leonvil <waddyleonvil@gmail.com>
2024-07-26 20:30:05 -0400

    Added pathfinding

--- Assets/Scripts/CustomerScript.cs
+++ Assets/Scripts/CustomerScript.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using Unity.Collections;
 using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class CustomerScript : MonoBehaviour {
@@ -10,6 +14,7 @@ public class CustomerScript : MonoBehaviour {
         ShoppingStopped,
         Alerted,
         Investigating,
+        Returning,
         Chasing,
         Stunned,
         Smelling
@@ -23,15 +28,21 @@ public class CustomerScript : MonoBehaviour {
     private CustomerState currentState;
     private GameObject player;
     private int currentPathIndex;
+    private int currentChasePathIndex;
+    private List<Vector3> pathVectorList;
     private bool hasLineOfSight = false;
     private Transform lastStoppingPoint;
+    private Transform investigationPoint;
 
     [Header("Speeds")]
     [SerializeField] private float shoppingSpeed = 5f;
+    [SerializeField] private float investigateSpeed = 2f;
+    [SerializeField] private float chaseSpeed = 3.1f;
+    [SerializeField] private float returnSpeed = 2f;
 
     void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animation>();
+        rb = transform.GetChild(0).gameObject.GetComponent<Rigidbody2D>();
+        animator = transform.GetChild(0).gameObject.GetComponent<Animation>();
         player = GameObject.FindGameObjectWithTag("Player");
         currentState = CustomerState.Shopping;
         StartCoroutine(FSM());
@@ -61,13 +72,20 @@ public class CustomerScript : MonoBehaviour {
                     break;
                 case CustomerState.Alerted:
                     animator.Play();
+                    SetTargetPosition(player.transform.position);
                     yield return new WaitForSeconds(0.5f);
                     currentState = CustomerState.Investigating;
                     break;
                 case CustomerState.Investigating:
                     Investigating();
                     break;
+                case CustomerState.Returning:
+                    //yield return new WaitForSeconds(2f);
+                    SetTargetPosition(pathPoints[0].position);
+                    Returning();
+                    break;
                 case CustomerState.Chasing:
+                Debug.Log("Chasing");
                     Chasing();
                     break;
                 case CustomerState.Stunned:
@@ -89,23 +107,27 @@ public class CustomerScript : MonoBehaviour {
 
         Vector2 direction = (targetPoint.position - transform.position).normalized;
 
-        if (Vector2.Distance(transform.position, player.transform.position) < 3) { // If customer detects player's smell, trigger Alerted state
-            currentState = CustomerState.Alerted;
+        if (Vector2.Distance(transform.position, player.transform.position) < 3f) { // If customer detects player
+            if (hasLineOfSight) {
+                currentState = CustomerState.Chasing;
+            } else {
+                currentState = CustomerState.Alerted;
+            }
+            
             return;
         }
 
 
-        if (direction != Vector2.zero) { // Rotate sprite along path
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
-        }
+        //if (direction != Vector2.zero) { // Rotate sprite along path
+        //    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //    transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+        //}
 
         /* foreach (Transform point in stoppingPoints) {
             if (point != null) {
                 if (Vector2.Distance(transform.position, point.position) < 0.1f && lastStoppingPoint != point) { // If customer reaches stopping point
                 lastStoppingPoint = point;
                     if (UnityEngine.Random.value <= 1f) {
-                        Debug.Log("WOO");
                         currentState = CustomerState.ShoppingStopped;
                     }
                 }
@@ -118,7 +140,6 @@ public class CustomerScript : MonoBehaviour {
     }
 
     IEnumerator ShoppingStopped() {
-        Debug.Log("Function entered");
         transform.Rotate(0, 0, -90f);
         bool lookingLeft = true;
         bool searchFinished = false;
@@ -159,11 +180,64 @@ public class CustomerScript : MonoBehaviour {
 
     void Investigating() {
         Debug.Log("Investigation started");
-        return;
+
+        if (Vector2.Distance(transform.position, player.transform.position) < 3 && hasLineOfSight) {
+            currentState = CustomerState.Chasing;
+             return;
+        }
+
+        if (pathVectorList != null) {
+            Vector3 targetPosition = pathVectorList[currentChasePathIndex];
+            if (Vector3.Distance(transform.position, targetPosition) > .5f) {
+                Vector3 moveDir = (targetPosition - transform.position).normalized;
+
+                transform.position = transform.position + moveDir * investigateSpeed * Time.deltaTime;
+            } else {
+                currentChasePathIndex++;
+                if (currentChasePathIndex >= pathVectorList.Count) {
+                    pathVectorList = null;
+                    if (Vector2.Distance(transform.position, player.transform.position) > 2.5f || !hasLineOfSight) { // They reach the last point and player isn't around
+                        currentState = CustomerState.Returning;
+                    }
+                }
+            }
+        }
+    }
+
+    void Returning() {
+        Debug.Log("Returning");
+
+        if (Vector2.Distance(transform.position, player.transform.position) < 3 && hasLineOfSight) {
+            currentState = CustomerState.Chasing;
+             return;
+        }
+
+        if (pathVectorList != null) {
+            Vector3 targetPosition = pathVectorList[currentChasePathIndex];
+            if (Vector3.Distance(transform.position, targetPosition) > .5f) {
+                Vector3 moveDir = (targetPosition - transform.position).normalized;
+
+                transform.position = transform.position + moveDir * returnSpeed * Time.deltaTime;
+            } else {
+                currentChasePathIndex++;
+                if (currentChasePathIndex >= pathVectorList.Count) {
+                    pathVectorList = null;
+                    currentPathIndex = 0;
+                    currentState = CustomerState.Shopping;
+                }
+            }
+        }
     }
 
     void Chasing() {
-        return;
+        Vector3 moveDir = (player.transform.position - transform.position).normalized;
+
+        if (Vector2.Distance(transform.position, player.transform.position) < 3 && hasLineOfSight) {
+            transform.position = transform.position + moveDir * chaseSpeed * Time.deltaTime;
+        } else if (!hasLineOfSight) {
+            SetTargetPosition(player.transform.position);
+            currentState = CustomerState.Investigating;
+        }
     }
 
     void Stunned() {
@@ -172,6 +246,21 @@ public class CustomerScript : MonoBehaviour {
 
     void Smelling() {
         return;
+    }
+
+    public void SetTargetPosition(Vector3 targetPosition) {
+        currentChasePathIndex = 0;
+        pathVectorList = Pathfinder.Instance.FindPath(transform.position, targetPosition);
+
+        if (pathVectorList != null) {
+            for (int i = 0; i < pathVectorList.Count - 1; i++) {
+                Debug.DrawLine(new Vector3(pathVectorList[i].x, pathVectorList[i].y), new Vector3(pathVectorList[i + 1].x, pathVectorList[i + 1].y), Color.green, 5f);
+            }
+        }
+
+        if (pathVectorList != null && pathVectorList.Count > 1) {
+            pathVectorList.RemoveAt(0);
+        }
     }
 
     private void OnDrawGizmos() {
